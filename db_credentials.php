f16d8f09c659a2bf13c95c86443dd0a9702abda7
ryanaustinolson <ryanaustinolson@gmail.com>
2018-04-22 13:38:00 -0500

    Initial Commit

--- db_credentials.php
+++ db_credentials.php
@@ -1,6 +1,7 @@
 <?php
-$servername = "sql305.epizy.com";
-$username = "epiz_21487459";
-$password = "Punisher";
-$dbname = "epiz_21487459_Final";
+/* Username: scofield9991@gmail.com Password: cs3380finalproject */
+$servername = "sql312.epizy.com";
+$username = "epiz_21975004";
+$password = "cs3380";
+$dbname = "epiz_21975004_final";
 ?>
